680716709f089b65ad42e334a823357b3490badc
olliezyan1i9k99-cat <olliezyan1i9k99-cat@mail.ru>
2020-03-21 18:11:21 +0300

    Change index.html and add .css with images

--- LikeFood/logic/__init__.py
+++ LikeFood/logic/__init__.py
@@ -5,8 +5,8 @@ from flask_login import LoginManager
 from flask_marshmallow import Marshmallow
 
 
-app = Flask(__name__, template_folder='render_template', static_folder='styles')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost/LikeFood'
+app = Flask(__name__, template_folder='render_template', static_folder="static")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:admin@localhost/LikeFood'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 SECRET_KEY = 'secret!'
 app.config.from_object(__name__)
